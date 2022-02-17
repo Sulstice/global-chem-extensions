30f1718b08221416de34b5988ace8d3199af466b
Sulstice <sharifsuliman1@gmail.com>
2022-02-17 18:42:28 -0500

    ss-master: push new version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 # ----
 setup(
     name="global_chem_extensions",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     license='MPL 2.0',
     author="Suliman Sharif",
